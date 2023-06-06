ccc472b1f5f042b92b5186cf115735caa99bd195
Ines Murtagh <imurtagh@itba.edu.ar>
2023-06-06 19:03:37 -0300

    Create script10.js

---
+++ Clases/script10.js
@@ -0,0 +1,61 @@
+console.log("hola"); // comentario
+/*comentario 
+multi linea
+*/
+
+// let nombre = "Benjamin"
+// var apellido = "Ochoa"
+// const edad = 20
+// camelCase
+//snake_case
+//kebab-case
+//l33t
+
+// alert("algo inteligente")//comando de salida - print
+// let nombreUsuario = prompt("ingrese su nombre")//comando de salida - input
+// alert("su nombre es " + nombreUsuario)
+
+//Tipos de datos
+//NaN (not a number)
+
+// int Numero = 10
+// str nombre = "Benjamin"
+
+//true - false
+//Truthy y falsy
+// + - * /
+console.log(1+1);
+console.log(1-1);
+console.log(1*1);
+console.log(1/1);
+
+console.log("Benjamin"+"Ochoa")
+console.log("11"-1)
+console.log("12"+1)
+
+console.log(10/5)
+console.log(10%5)
+console.log(3**2)
+
+console.log(1==1)
+console.log(1!=1)
+console.log("1"==1)
+console.log("1"===1)
+console.log("1"!==1)
+
+let edadMinima = 17
+
+console.log(++edadMinima)
+
+let edadARestar = prompt("Ingrese su edad:")
+console.log(typeof(edadARestar))
+alert("Su edad el año pasado era de: " + (pareseInt (edadARestar - 1)))
+
+
+/* 
+Ejercicios para practicar:
+1) Pedir un numero y mostrar su doble, su mitad, el siguiente y el anterior
+2)Pedir nombre y apellidpo y mostrar la siguiente frase: "Su nombre es (Nombre) y su apellido es (apellido)"
+3)Pedir al usuario 2 numeros y mostrar true si el primero es mayor al segundo y false si el segundo es mayor.
+4)Pedir al usuario un numero y mostrar 0 si es par y 1 si es impar
+*/
